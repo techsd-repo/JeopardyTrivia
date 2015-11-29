8d84be178785bd733a3cd07b347baa163b582ae2
techsd-repo <techlcsd@gmail.com>
2015-11-29 11:02:50 -0500

    Changed Nav Bar color(temporary, will change later)

---
+++ Jeopardy Trivia/TableViewFindMatchType.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewFindMatchType.swift
+//  Jeopardy Trivia
+//
+//  Created by Alex Folster on 11/22/15.
+//  Copyright © 2015 Alex Folster. All rights reserved.
+//
+
+import Foundation
