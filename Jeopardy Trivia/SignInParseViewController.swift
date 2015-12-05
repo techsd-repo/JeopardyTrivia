9fbd3fe1e77ad7040a5cfc0592a1e1574bf52d9b
techsd-repo <techlcsd@gmail.com>
2015-12-05 18:15:19 -0500

    Added login logic Still need to create main view controller first

--- Jeopardy Trivia/SignInParseViewController.swift
+++ Jeopardy Trivia/SignInParseViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import Parse
 
 class SignInParseViewController: UIViewController {
 
+    @IBOutlet weak var userField: UITextField!
+    @IBOutlet weak var passField: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +28,37 @@ class SignInParseViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.userField.resignFirstResponder()
+        self.passField.resignFirstResponder()
+    }
 
+    @IBAction func logInAction(sender: AnyObject) {
+        //Log in user here
+        let userf = self.userField.text
+        let passf = self.passField.text
+        PFUser.logInWithUsernameInBackground(userf!, password: passf!) { (user, error) -> Void in
+            if user != nil {
+                //Yay! Logged in successfully
+                
+                //FIXME: Add segue to Main screen
+            } else {
+                //Error.. hmm...
+                let alert = UIAlertController(title: "Error:", message: "\(error) Try changing your username or password", preferredStyle: .Alert)
+                let okAction = UIAlertAction(title: "Ok", style: .Default, handler: { (action) -> Void in
+                    
+                })
+                alert.addAction(okAction)
+                
+                self.presentViewController(alert, animated: true, completion: { () -> Void in
+                    
+                })
+            }
+            
+        }
+    }
+    
+    
     /*
     // MARK: - Navigation
 
