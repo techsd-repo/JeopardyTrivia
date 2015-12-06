15b7db73595422a27e38a953199396235c15217e
techsd-repo <techlcsd@gmail.com>
2015-12-06 09:39:06 -0500

    Added sidebar menu

--- Jeopardy Trivia/MainUIViewController.swift
+++ Jeopardy Trivia/MainUIViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class MainUIViewController: UIViewController {
 
+    @IBOutlet weak var menuButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
         // Do any additional setup after loading the view.
     }
 
